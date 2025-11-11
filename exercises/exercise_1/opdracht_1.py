c0f39ff5c0d046dfe09f866946424d0aba89507e
CasperSchaminee <schaminee.casper@ah.nl>
2025-11-11 15:02:10 +0000

    add function for list with max 2 items output

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,8 @@
 def likes(team: list) -> str:
-    return True
+    if len(team) == 0:
+        return "no one likes this"
+    elif len(team) == 1:
+        return f"{team[0]} likes this"
+    
+    elif len(team) == 2:
+        return f"{team[0]} and {team[1]} likes this"
